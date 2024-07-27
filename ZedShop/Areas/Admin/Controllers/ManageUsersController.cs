415c690fe2fece7d554877073abed8bdfc4b2506
Behnamzk <zeraatkar.behnam98@gmail.com>
2024-07-27 19:49:51 +0330

    update Admin pannel

---
+++ ZedShop/Areas/Admin/Controllers/ManageUsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZedShop.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ManageUsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
